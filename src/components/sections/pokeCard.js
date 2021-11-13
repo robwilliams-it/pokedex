7f75aed8389843527e415e9c16a273178300f2ea
robwilliams-it <robwilliams.it@gmail.com>
2021-11-12 23:20:10 -0600

    add type color

--- src/components/sections/pokeCard.js
+++ src/components/sections/pokeCard.js
@@ -6,6 +6,28 @@ import { CardActionArea } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 
+const typeColors = {
+    'Bug' : '#A6B91A',
+    'Dark' : '#705746',
+    'Dragon' : '#6F35FC',
+    'Electric' : '#F7D02C',
+    'Fairy' : '#D685AD',
+    'Fighting' : '#C22E28',
+    'Fire' : '#EE8130',
+    'Flying' : '#A98FF3' ,
+    'Ghost' : '#735797',
+    'Grass' : '#7AC74C',
+    'Ground' : '#E2BF65',
+    'Ice' : '#96D9D6',
+    'Normal': '#A8A77A',
+    'Poison' : '#A33EA1',
+    'Psychic' : '#F95587',
+    'Rock' : '#B6A136',
+    'Steel' : '#B7B7CE',
+    'Water' : '#6390F0',
+
+}
+
 const PokeCard =(props)=> {
     const pokeProps = props.pokeData || {};
     const handleClick = props.handleClick;
@@ -22,7 +44,7 @@ const PokeCard =(props)=> {
         } else {
             return (
                 <Typography gutterBottom variant="h3" component="div">
-                    Click It!
+                    Lets See What Wee Caught!
                 </Typography>
             )
         }
@@ -37,9 +59,13 @@ const PokeCard =(props)=> {
             }
             return (
                 pokeProps.type.map((type)=>(
-                <Typography gutterBottom variant="h5" component="div">
-                    { type }
-                </Typography>
+                <Card sx={{backgroundColor: `${typeColors[type]}` }}>
+                    <Container>
+                        <Typography gutterBottom variant="h5" component="div" sx={{color:'white'}}>
+                            { type }
+                        </Typography>
+                    </Container>
+                </Card>
                 ))
             )
         }
@@ -57,7 +83,7 @@ const PokeCard =(props)=> {
                 <Grid
                     container
                     direction="column"
-                    justifyContent="center"
+                    justifyContent="space-around"
                     alignItems="center"
                     sx={{height: '100%'}}
                 >
@@ -86,6 +112,7 @@ const PokeCard =(props)=> {
                         image= {pokeProps.image || '../images/pokeball.png'}
                     />
 
+                    <br/>
                 </Grid>
             {/* </CardActionArea> */}
         </Card>
